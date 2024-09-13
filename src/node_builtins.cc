6f661dab7f6aa4c3925920377a17b0ba9e3c3ac7
Josh Fried <joshuafried@gmail.com>
2024-09-13 15:18:31 +0000

    builtins_module_paths_not_shareable.patch

--- src/node_builtins.cc
+++ src/node_builtins.cc
@@ -36,6 +36,7 @@ using v8::Value;
 BuiltinLoader::BuiltinLoader()
     : config_(GetConfig()), code_cache_(std::make_shared<BuiltinCodeCache>()) {
   LoadJavaScriptSource();
+#ifndef NODE_BUILTIN_MODULES_PATH
 #ifdef NODE_SHARED_BUILTIN_CJS_MODULE_LEXER_LEXER_PATH
   AddExternalizedBuiltin(
       "internal/deps/cjs-module-lexer/lexer",
@@ -64,6 +65,7 @@ BuiltinLoader::BuiltinLoader()
     "internal/deps/acorn/acorn-walk/dist/walk",
     STRINGIFY(NODE_SHARED_BUILTIN_ACORN_WALK_PATH));
 #endif  // NODE_SHARED_BUILTIN_ACORN_WALK_PATH
+#endif
 
 }
 
